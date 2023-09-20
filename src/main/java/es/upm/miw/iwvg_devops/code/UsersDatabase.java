abd661e12b361b8a8f655c62da14fd786bba00d6
Mario <mario10_bd@hotmail.com>
2023-09-20 20:03:03 +0200

    ilegal fractions in database Function findUserFamilyNameInitialBySomeProperFraction #10

--- src/main/java/es/upm/miw/iwvg_devops/code/UsersDatabase.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/UsersDatabase.java
@@ -35,8 +35,8 @@ public class UsersDatabase {
         );
 
         List<Fraction> fractions6 = List.of(
-                new Fraction(0, 0),
-                new Fraction(1, 0),
+                new Fraction(0, 1),
+                new Fraction(1, -1),
                 new Fraction(1, 1)
         );
 
